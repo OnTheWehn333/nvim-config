53f06493c0277cae146a788e3a7d1fa78ba4b449
noahbalboa66 <noahwehn@gmail.com>
2024-04-25 15:49:13 -0500

    Delete fugitive and gv. Have other git tools now

--- lua/noahbalboa66/plugins/fugitive.lua
+++ lua/noahbalboa66/plugins/fugitive.lua
@@ -1,9 +1,12 @@
+-- return {
+--     'tpope/vim-fugitive',
+--     config = function()
+--         vim.keymap.set("n", "<leader>gs", "<cmd>:tab Git<CR>", { desc = 'Git Status' });
+--         vim.keymap.set("n", "<leader>gF", ":Git fetch", { desc = 'Git Fetch' });
+--         -- TODO: Try adding -u for setting upstream for the branch
+--         vim.keymap.set("n", "<leader>gpn", ":Git -c push.default=current push", { desc = 'Git Push New branch' });
+--     end
+-- }
 return {
-    'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set("n", "<leader>gs", "<cmd>:tab Git<CR>", { desc = 'Git Status' });
-        vim.keymap.set("n", "<leader>gF", ":Git fetch", { desc = 'Git Fetch' });
-        -- TODO: Try adding -u for setting upstream for the branch
-        vim.keymap.set("n", "<leader>gpn", ":Git -c push.default=current push", { desc = 'Git Push New branch' });
-    end
+
 }
