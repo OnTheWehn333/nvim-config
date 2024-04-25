ab2a370019840057d27451fa03793ba3e1f872a0
noahbalboa66 <noahwehn@gmail.com>
2024-04-25 15:49:20 -0500

    Add trouble

---
+++ lua/noahbalboa66/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        -- local trouble = require("trouble.providers.telescope")
+        -- vim.keymap.set("n", "<leader>sT", trouble.open_with_trouble, { desc = 'Open Trouble with Telescope' })
+    end,
+}
