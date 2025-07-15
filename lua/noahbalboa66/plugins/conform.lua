b00480dcc6d63173200d330f285bb6c7c4fb88bc
Noah Wehn <noahwehn@gmail.com>
2025-07-15 18:53:20 -0500

    fix conform
    Had a function that was overriden default behaviour I guess

--- lua/noahbalboa66/plugins/conform.lua
+++ lua/noahbalboa66/plugins/conform.lua
@@ -1,53 +1,49 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            -- Customize or remove this keymap to your liking
-            "<leader>f",
-            function()
-                require("conform").format({ async = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    -- This will provide type hinting with LuaLS
-    ---@module "conform"
-    ---@type conform.setupOpts
-    opts = {
-        -- Define your formatters
-        formatters_by_ft = {
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            javascript = {},
-            typescript = {},
-        },
-        -- Set default options
-        default_format_opts = {
-            lsp_format = "fallback",
-        },
-        -- Set up format-on-save
-        format_on_save = { timeout_ms = 500 },
-        -- Customize formatters
-        formatters = {
-            shfmt = {
-                prepend_args = { "-i", "2" },
-            },
-        },
-    },
-    init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-    end,
-    config = function()
-        require("conform").setup({
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                lsp_format = "fallback",
-            },
-        })
-    end
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_format = "fallback" })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			cs = { "csharpier" },
+			javascript = {},
+			typescript = {},
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+			-- csharpier = {
+			-- 	command = "dotnet",
+			-- 	args = { "csharpier", "--write-stdout" },
+			-- },
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
 }
