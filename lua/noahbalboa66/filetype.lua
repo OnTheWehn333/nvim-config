66626034472047be345d6dba1314b2c54360e783
noahbalboa66 <noahwehn@gmail.com>
2025-07-18 19:46:08 -0500

    fix nix width

--- lua/noahbalboa66/filetype.lua
+++ lua/noahbalboa66/filetype.lua
@@ -3,3 +3,14 @@ vim.filetype.add({
 		fga = "yaml",
 	},
 })
+
+-- Filetype-specific settings
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "nix",
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end,
+})
