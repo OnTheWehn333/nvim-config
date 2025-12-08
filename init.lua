1ae7ea8977769bac56a559d09197bfaef027dceb
noahbalboa66 <noahwehn@gmail.com>
2025-12-08 00:02:39 -0600

    osc

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.g.terminal_emulator = "zsh"
-vim.g.clipboard = "osc52"
+vim.opt.clipboard = "unnamedplus"
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
