13ac3092dba368620342dd2d8d77ee1efd2dfeb0
noahbalboa66 <noahwehn@gmail.com>
2026-03-14 22:04:51 -0500

    fix again?

--- init.lua
+++ init.lua
@@ -1,6 +1,36 @@
 vim.g.mapleader = " "
 vim.g.terminal_emulator = "zsh"
 
+vim.g.clipboard = "osc52"
+
+local function osc52_copy(reg)
+	return function(lines)
+		local s = table.concat(lines, "\n")
+		local b64 = vim.base64.encode(s)
+		local osc = string.format("\x1b]52;%s;%s\x07", reg == "*" and "p" or "c", b64)
+		if vim.env.TMUX then
+			io.stdout:write("\x1bPtmux;\x1b" .. osc .. "\x1b\\")
+		else
+			io.stdout:write(osc)
+		end
+	end
+end
+vim.g.clipboard = {
+	name = "OSC 52",
+	copy = {
+		["+"] = osc52_copy("+"),
+		["*"] = osc52_copy("*"),
+	},
+	paste = {
+		["+"] = function()
+			return { vim.fn.getreg("+", true, true), vim.fn.getregtype("+") }
+		end,
+		["*"] = function()
+			return { vim.fn.getreg("*", true, true), vim.fn.getregtype("*") }
+		end,
+	},
+}
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
 	vim.fn.system({
